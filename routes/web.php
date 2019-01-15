1fa38c73132120aa186997a2159b340ad187d366
yarmat <yarmatsu@gmail.com>
2019-01-15 16:35:55 +0200

    Create test db and setup unit testing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('index');
 
-Auth::routes();
+// Auth::routes(); disabled, because i use only 3 routes [login(get|post), logout(post)] for my Portfolio
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
