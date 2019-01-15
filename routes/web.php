3a27596269489252611ff374d7068db4beef8f29
yarmat <yarmatsu@gmail.com>
2019-01-15 17:57:47 +0200

    Create main layout

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('index');
+Route::get('/', 'HomeController@index')->name('index');
 
 // Auth::routes(); disabled, because i use only 3 routes [login(get|post), logout(post)] for my Portfolio
 
