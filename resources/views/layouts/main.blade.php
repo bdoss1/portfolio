7f63469c6149d14c536db12ab83cffbacbf5a8bb
yarmat <yarmatsu@gmail.com>
2019-02-05 15:55:36 +0200

    Seo up speed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -159,7 +159,7 @@
     };
 </script>
 {{--<script src="https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" async defer></script>--}}
-{{--<script src="{{ asset('js/app.js') }}"></script>--}}
+<script src="{{ asset('js/app.js') }}"></script>
 @yield('after.script')
 </body>
 </html>
